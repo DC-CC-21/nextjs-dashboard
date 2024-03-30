1889e64c2a46931612b593a7945dbf112ce07590
DC-CC-21 <handstands1000@gmail.com>
2024-03-30 14:44:49 -0400

    nextjs project

---
+++ app/ui/fonts.ts
@@ -0,0 +1,4 @@
+import {Inter, Imperial_Script, Lusitana} from 'next/font/google';
+export const inter = Inter({subsets: ['latin']});
+export const imperialScript = Imperial_Script({subsets: ['latin'], weight: ['400']});
+export const lusitana = Lusitana({subsets: ['latin'], weight: ['400']})
